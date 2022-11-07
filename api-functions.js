a24850cf169836cd0935852841043bf059007d51
Benedict <benedictryan80@gmail.com>
2022-11-08 00:13:25 +0700

    add dynamic crud feature

--- api-functions.js
+++ api-functions.js
@@ -1,19 +1,25 @@
 const getProducts = async () => {
-	const res = await fetch('http://localhost:3000/api/products', {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
+	const res = await fetch(
+		'https://next-auth-mongodb-redux-usages.vercel.app/api/products',
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
 		}
-	});
+	);
 	const allProducts = await res.json();
 	return allProducts.data;
 };
 
 const addNewProduct = async (payload) => {
-	const res = await fetch('http://localhost:3000/api/products', {
-		method: 'POST',
-		body: JSON.stringify(payload)
-	});
+	const res = await fetch(
+		'https://next-auth-mongodb-redux-usages.vercel.app/api/products',
+		{
+			method: 'POST',
+			body: JSON.stringify(payload)
+		}
+	);
 	const newProduct = await res.json();
 	console.log(newProduct);
 	return payload;
@@ -21,12 +27,15 @@ const addNewProduct = async (payload) => {
 
 const deleteSingleProduct = async (payload) => {
 	console.log(payload);
-	const res = await fetch(`http://localhost:3000/api/product/${payload}`, {
-		method: 'DELETE',
-		headers: {
-			'Content-Type': 'application/json'
+	const res = await fetch(
+		`https://next-auth-mongodb-redux-usages.vercel.app/api/product/${payload}`,
+		{
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
 		}
-	});
+	);
 	const deleteProduct = await res.json();
 	console.log(deleteProduct);
 	return payload;
